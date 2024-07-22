8094e983df3d2682bcf64230d225f30170fcca32
mohammedshanan <mohammedshana12@gmail.com>
2024-07-22 22:23:40 +0300

    added files

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,30 +1,33 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+""" Square module. """
 
 
-class square:
-
+class Square():
+    """ Square class. """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        for (key, value) in kwargs.items():
+        """ Square class initialized """
+        for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        return self.width * 2 + self.height * 2
+    def permiter_of_my_square(self):
+        """ Permiter of the square """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return '{}/{}'.format(self.width, self.height)
+        """ String representation of the square instance """
+        return "{}/{}".format(self.width, self.height)
 
 
-if __name__ == '__main__':
-
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    """ Create Square instance """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
